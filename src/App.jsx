76ea5ad24585bdb6e3f60acf2df50042f28cf869
llllllll-l <vincent.werelius@gmail.com>
2024-02-27 09:55:37 +0100

    react-use-effect-dependencies-practice done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DataList from "./components/DataList.jsx";
 import SelectTypeForm from "./components/SelectTypeForm.jsx";
 import "./App.css";
@@ -8,10 +8,28 @@ export default function App() {
 
   const [data, setData] = useState(null);
 
-  console.log({ data });
+  //console.log({ data });
 
   // Write code here.
   //
+  useEffect(() => {
+    const fetchData = async () => {
+      //console.log("fetchData");
+      try {
+        if (dataType) {
+          //console.log(`Fetching data for ${dataType}`);
+          const req = await fetch(`https://swapi.dev/api/${dataType}/`);
+          const res = await req.json();
+          //console.log(`Data for ${dataType}:`, jsonData);
+          setData(res);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [dataType]);
 
   return (
     <div>
--- src/components/SelectTypeForm.jsx
+++ src/components/SelectTypeForm.jsx
@@ -1,10 +1,14 @@
-function SelectTypeForm(props) {
+function SelectTypeForm({ setDataType }) {
   // Write code here...
+  const handleSelectChange = (event) => {
+    const selectedDataType = event.target.value;
+    setDataType(selectedDataType);
+  };
 
   return (
     <form className="three-column-grid__expand-two gap-md">
       <label htmlFor="type">Data Type</label>
-      <select id="type" name="type">
+      <select id="type" name="type" onChange={handleSelectChange}>
         <option value="">Please select...</option>
         <option value="people">People</option>
         <option value="planets">Planets</option>
